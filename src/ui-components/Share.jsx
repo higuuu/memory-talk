74799427d7dc45a11da7e4d73b647c6cc41b6d8d
HiguchiShuya <higuzeroggw@gmail.com>
2022-01-05 23:36:47 +0900

    [add]シェア用のコンポーネントのファイルを追加

---
+++ src/ui-components/Share.jsx
@@ -0,0 +1,247 @@
+/***************************************************************************
+ * The contents of this file were generated with Amplify Studio.           *
+ * Please refrain from making any modifications to this file.              *
+ * Any changes to this file will be overwritten when running amplify pull. *
+ **************************************************************************/
+
+/* eslint-disable */
+import React from "react";
+import { getOverrideProps } from "@aws-amplify/ui-react/internal";
+import { Flex, Icon, Text, View } from "@aws-amplify/ui-react";
+export default function Share(props) {
+  const { overrides: overridesProp, ...rest } = props;
+  const overrides = { ...overridesProp };
+  return (
+    <Flex
+      gap="80px"
+      direction="row"
+      width="547px"
+      height="52px"
+      alignItems="flex-start"
+      position="relative"
+      padding="0px 0px 0px 0px"
+      {...rest}
+      {...getOverrideProps(overrides, "Flex")}
+    >
+      <Flex
+        gap="16px"
+        direction="row"
+        alignItems="flex-start"
+        shrink="0"
+        height="24px"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Flex.Flex[0]")}
+      >
+        <View
+          width="24px"
+          height="24px"
+          shrink="0"
+          overflow="hidden"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Flex.Flex[0].View[0]")}
+        >
+          <Icon
+            width="24px"
+            height="24px"
+            pathData=""
+            viewBox={{ minX: 0, minY: 0, width: 24, height: 24 }}
+            position="absolute"
+            left="0px"
+            top="0px"
+            {...getOverrideProps(overrides, "Flex.Flex[0].View[0].Icon[0]")}
+          ></Icon>
+          <Icon
+            width="16px"
+            height="16px"
+            pathData="M6.59 5.17L1.41 0L0 1.41L5.17 6.58L6.59 5.17ZM10.5 0L12.54 2.04L0 14.59L1.41 16L13.96 3.46L16 5.5L16 0L10.5 0ZM10.83 9.41L9.42 10.82L12.55 13.95L10.5 16L16 16L16 10.5L13.96 12.54L10.83 9.41L10.83 9.41Z"
+            viewBox={{ minX: 0, minY: 0, width: 16, height: 16 }}
+            color="rgba(13.000000175088644,26.000000350177288,38.0000015348196,1)"
+            position="absolute"
+            left="4px"
+            top="4px"
+            {...getOverrideProps(overrides, "Flex.Flex[0].View[0].Icon[1]")}
+          ></Icon>
+        </View>
+        <Text
+          fontFamily="Inter"
+          fontSize="16px"
+          fontWeight="400"
+          color="rgba(13.000000175088644,26.000000350177288,38.0000015348196,1)"
+          lineHeight="24px"
+          textAlign="left"
+          display="flex"
+          direction="column"
+          justifyContent="flex-start"
+          letterSpacing="0.010000000000000009px"
+          shrink="0"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          children="99"
+          {...getOverrideProps(overrides, "Flex.Flex[0].Text[0]")}
+        ></Text>
+      </Flex>
+      <Flex
+        gap="16px"
+        direction="row"
+        alignItems="flex-start"
+        shrink="0"
+        height="24px"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Flex.Flex[1]")}
+      >
+        <View
+          width="24px"
+          height="24px"
+          shrink="0"
+          overflow="hidden"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Flex.Flex[1].View[0]")}
+        >
+          <Icon
+            width="24px"
+            height="24px"
+            pathData=""
+            viewBox={{ minX: 0, minY: 0, width: 24, height: 24 }}
+            position="absolute"
+            left="0px"
+            top="0px"
+            {...getOverrideProps(overrides, "Flex.Flex[1].View[0].Icon[0]")}
+          ></Icon>
+          <Icon
+            width="20px"
+            height="18.350000381469727px"
+            pathData="M14.5 0C12.76 0 11.09 0.81 10 2.09C8.91 0.81 7.24 0 5.5 0C2.42 0 0 2.42 0 5.5C0 9.28 3.4 12.36 8.55 17.04L10 18.35L11.45 17.03C16.6 12.36 20 9.28 20 5.5C20 2.42 17.58 0 14.5 0ZM10.1 15.55L10 15.65L9.9 15.55C5.14 11.24 2 8.39 2 5.5C2 3.5 3.5 2 5.5 2C7.04 2 8.54 2.99 9.07 4.36L10.94 4.36C11.46 2.99 12.96 2 14.5 2C16.5 2 18 3.5 18 5.5C18 8.39 14.86 11.24 10.1 15.55Z"
+            viewBox={{
+              minX: 0,
+              minY: 0,
+              width: 20,
+              height: 18.349998474121094,
+            }}
+            color="rgba(13.000000175088644,26.000000350177288,38.0000015348196,1)"
+            position="absolute"
+            left="2px"
+            top="3px"
+            {...getOverrideProps(overrides, "Flex.Flex[1].View[0].Icon[1]")}
+          ></Icon>
+        </View>
+        <Flex
+          gap="16px"
+          direction="row"
+          alignItems="flex-start"
+          shrink="0"
+          height="24px"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          {...getOverrideProps(overrides, "Flex.Flex[1].Flex[0]")}
+        >
+          <View
+            width="24px"
+            height="24px"
+            shrink="0"
+            overflow="hidden"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            {...getOverrideProps(overrides, "Flex.Flex[1].Flex[0].View[0]")}
+          >
+            <Icon
+              width="24px"
+              height="24px"
+              pathData=""
+              viewBox={{ minX: 0, minY: 0, width: 24, height: 24 }}
+              position="absolute"
+              left="0px"
+              top="0px"
+              {...getOverrideProps(
+                overrides,
+                "Flex.Flex[1].Flex[0].View[0].Icon[0]"
+              )}
+            ></Icon>
+            <Icon
+              width="18px"
+              height="15px"
+              pathData="M7 4L7 0L0 7L7 14L7 9.9C12 9.9 15.5 11.5 18 15C17 10 14 5 7 4Z"
+              viewBox={{ minX: 0, minY: 0, width: 18, height: 15 }}
+              color="rgba(13.000000175088644,26.000000350177288,38.0000015348196,1)"
+              position="absolute"
+              left="3px"
+              top="5px"
+              {...getOverrideProps(
+                overrides,
+                "Flex.Flex[1].Flex[0].View[0].Icon[1]"
+              )}
+            ></Icon>
+          </View>
+          <Text
+            fontFamily="Inter"
+            fontSize="16px"
+            fontWeight="400"
+            color="rgba(13.000000175088644,26.000000350177288,38.0000015348196,1)"
+            lineHeight="24px"
+            textAlign="left"
+            display="flex"
+            direction="column"
+            justifyContent="flex-start"
+            letterSpacing="0.010000000000000009px"
+            shrink="0"
+            position="relative"
+            padding="0px 0px 0px 0px"
+            children="99"
+            {...getOverrideProps(overrides, "Flex.Flex[1].Flex[0].Text[0]")}
+          ></Text>
+        </Flex>
+        <Text
+          fontFamily="Inter"
+          fontSize="16px"
+          fontWeight="400"
+          color="rgba(13.000000175088644,26.000000350177288,38.0000015348196,1)"
+          lineHeight="24px"
+          textAlign="left"
+          display="flex"
+          direction="column"
+          justifyContent="flex-start"
+          letterSpacing="0.010000000000000009px"
+          shrink="0"
+          position="relative"
+          padding="0px 0px 0px 0px"
+          children="99"
+          {...getOverrideProps(overrides, "Flex.Flex[1].Text[0]")}
+        ></Text>
+      </Flex>
+      <View
+        width="24px"
+        height="24px"
+        shrink="0"
+        overflow="hidden"
+        position="relative"
+        padding="0px 0px 0px 0px"
+        {...getOverrideProps(overrides, "Flex.View[0]")}
+      >
+        <Icon
+          width="24px"
+          height="24px"
+          pathData=""
+          viewBox={{ minX: 0, minY: 0, width: 24, height: 24 }}
+          position="absolute"
+          left="0px"
+          top="0px"
+          {...getOverrideProps(overrides, "Flex.View[0].Icon[0]")}
+        ></Icon>
+        <Icon
+          width="18px"
+          height="19.920000076293945px"
+          pathData="M15 14.08C14.24 14.08 13.56 14.38 13.04 14.85L5.91 10.7C5.96 10.47 6 10.24 6 10C6 9.76 5.96 9.53 5.91 9.3L12.96 5.19C13.5 5.69 14.21 6 15 6C16.66 6 18 4.66 18 3C18 1.34 16.66 0 15 0C13.34 0 12 1.34 12 3C12 3.24 12.04 3.47 12.09 3.7L5.04 7.81C4.5 7.31 3.79 7 3 7C1.34 7 0 8.34 0 10C0 11.66 1.34 13 3 13C3.79 13 4.5 12.69 5.04 12.19L12.16 16.35C12.11 16.56 12.08 16.78 12.08 17C12.08 18.61 13.39 19.92 15 19.92C16.61 19.92 17.92 18.61 17.92 17C17.92 15.39 16.61 14.08 15 14.08ZM15 2C15.55 2 16 2.45 16 3C16 3.55 15.55 4 15 4C14.45 4 14 3.55 14 3C14 2.45 14.45 2 15 2ZM3 11C2.45 11 2 10.55 2 10C2 9.45 2.45 9 3 9C3.55 9 4 9.45 4 10C4 10.55 3.55 11 3 11ZM15 18.02C14.45 18.02 14 17.57 14 17.02C14 16.47 14.45 16.02 15 16.02C15.55 16.02 16 16.47 16 17.02C16 17.57 15.55 18.02 15 18.02Z"
+          viewBox={{ minX: 0, minY: 0, width: 18, height: 19.919998168945312 }}
+          color="rgba(13.000000175088644,26.000000350177288,38.0000015348196,1)"
+          position="absolute"
+          left="3px"
+          top="2px"
+          {...getOverrideProps(overrides, "Flex.View[0].Icon[1]")}
+        ></Icon>
+      </View>
+    </Flex>
+  );
+}
