c37fa7aa1a3217a756b12fdcafaf15c24eb3f210
HiguchiShuya <higuzeroggw@gmail.com>
2022-01-05 23:35:48 +0900

    [add]シェア用のコンポーネントを追加

--- src/ui-components/index.js
+++ src/ui-components/index.js
@@ -4,5 +4,6 @@
  * Any changes to this file will be overwritten when running amplify pull. *
  **************************************************************************/
 
+export { default as Share } from "./Share";
 export { default as CardHomeTalkCollection } from "./CardHomeTalkCollection";
 export { default as CardHomeTalk } from "./CardHomeTalk";
