759395314870ffc6db4e94349ccab2750993876c
HiguchiShuya <higuzeroggw@gmail.com>
2022-01-06 18:52:26 +0900

    [mod]追加したshareコンポネントに表示

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import { CardHomeTalkCollection } from './ui-components';
-
+import { Share } from './ui-components'
+ 
 function App() {
   return (
     <div className="App">
       <CardHomeTalkCollection />
+      <Share />
     </div>
   );
 }
